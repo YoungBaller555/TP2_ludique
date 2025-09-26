d7f22b49f6ebe9153ee3ef00946332a9d79d7699
vasile <ballmaster@gmail.com>
2025-09-26 13:16:02 -0400

    tests2

--- projet/tp2_vds/character_body_2d.gd
+++ projet/tp2_vds/character_body_2d.gd
@@ -1,1 +1,19 @@
 extends CharacterBody2D
+
+@export var speed = 1200
+@export var jump_speed = -1800
+@export var gravity = 4000
+
+
+func _physics_process(delta):
+	# Add gravity every frame
+	velocity.y += gravity * delta
+
+	# Input affects x axis only
+	velocity.x = Input.get_axis("walk_left", "walk_right") * speed
+
+	move_and_slide()
+
+	# Only allow jumping when on the ground
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
